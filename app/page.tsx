5e5a7dc7cfa06375f509a3c97e18cda29887e5bf
salman261002 <salmanalfarisi261002@gmail.com>
2024-01-28 16:01:19 +0700

    add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,59 @@
+import Link from "next/link";
+import Image from "next/image";
+
+// Fonts
+import { Poppins } from "next/font/google";
+
+// Icons
+import { FaArrowRight, FaAsterisk } from "react-icons/fa";
+
+// Assets
+import Memoji01 from "@/public/assets/memojis/salman.jpg";
+import { FaArrowRightLong } from "react-icons/fa6";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+});
+
 export default function Home() {
   return (
-    <main className="flex min-h-[calc(100vh-80px)] md:min-h-[calc(100vh-93px)] w-full flex-col items-center justify-between py-6">
-      <h1 className="text-4xl">Hello World</h1>
+    <main className="flex min-h-[calc(100vh-80px)] md:min-h-[calc(100vh-88px)] w-full flex-col md:flex-row pad-x py-6 md:pt-12 md:pb-0">
+      <div className="flex flex-col text-center items-center md:items-end md:text-left w-full md:w-1/2 pr-0 md:pr-8 h-auto gap-y-6 lg:gap-y-8">
+        {/* Main Text */}
+        <h1
+          className={`text-2xl md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-6xl ${poppins.className}`}
+        >
+          Transform Your Business{" "}
+          <FaArrowRight className="inline-block w-4 h-4 lg:w-8 lg:h-8 text-background/50" />{" "}
+          With Delightful Digital Experiences{" "}
+          <FaAsterisk className="inline-block w-4 h-4 lg:w-8 lg:h-8 text-primary" />
+        </h1>
+
+        {/* Sub Text */}
+        <div className="flex justify-center md:justify-end gap-x-4">
+          <FaArrowRightLong className="w-5 h-5 text-background hidden md:block" />
+          <p className="text-base text-background/80 w-[90%] md:text-lg lg:text-xl font-light">
+            We are a digital product design and development company. We help
+            businesses build delightful digital experiences.
+          </p>
+        </div>
+
+        {/* CTAs */}
+        <div className="flex items-center justify-center md:justify-start">
+          <Link
+            href="/"
+            className="border-b-2 pb-1 group flex items-center w-fit anim gap-x-3 text-primary-foreground"
+          >
+            <div className="text-base md:text-lg uppercase font-medium">
+              Learn More
+            </div>
+          </Link>
+        </div>
+
+        {/* Feature */}
+
+      </div>
     </main>
   );
 }
--- components/navigations/navbar.tsx
+++ components/navigations/navbar.tsx
@@ -5,7 +5,7 @@ import NavItem from "./navitem";
 const Navbar = () => {
   return (
     <header className="w-full flex items-center pad-x relative">
-      <nav className="flex w-full items-center py-6 gap-x-8 md:px-8">
+      <nav className="flex w-full items-center py-6 gap-x-8">
         {/* Logo */}
         <div className="w-fit flex items-start md:items-center gap-x-1 group peer cursor-pointer">
           <h1 className="text-xl md:text-2xl uppercase z-10 bg-foreground leading-none md:leading-normal">
